27f91a297f6002f484102613b91ed5605e2b6972
AidasV1 <aidasv523@gmail.com>
2021-10-10 18:14:35 +0300

    design improvements

--- app/src/main/java/edu/ktu/myfirstapplication/FirstActivity.java
+++ app/src/main/java/edu/ktu/myfirstapplication/FirstActivity.java
@@ -3,9 +3,14 @@ package edu.ktu.myfirstapplication;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.view.menu.MenuView;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,7 +21,7 @@ public class FirstActivity extends AppCompatActivity {
     private Button myButton;
     private Button skelbimai;
     private Toolbar myToolbar;
-    private TextView myTextView;
+    private TextView link;
     private Button secondActivityButton;
     private Context context = this;
 
@@ -32,7 +37,10 @@ public class FirstActivity extends AppCompatActivity {
 
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
-  //      myTextView = (TextView) findViewById(R.id.textView);
+        link = (TextView) findViewById(R.id.link);
+        String linkText = "Visit this <a href='https://google.com'>Google</a> web page.";
+        link.setText(Html.fromHtml(linkText));
+        link.setMovementMethod(LinkMovementMethod.getInstance());
 
         skelbimai.setOnClickListener(skelbimaiClick);
         secondActivityButton.setOnClickListener(startSecondActivity);
@@ -99,6 +107,19 @@ public class FirstActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
 
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.appbarmenu, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView =
+                (SearchView) searchItem.getActionView();
+
+        // Configure the search info and add any event listeners...
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
 
--- app/src/main/java/edu/ktu/myfirstapplication/Skelbimai.java
+++ app/src/main/java/edu/ktu/myfirstapplication/Skelbimai.java
@@ -1,13 +1,14 @@
 package edu.ktu.myfirstapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 
 
 public class Skelbimai extends AppCompatActivity {
@@ -21,9 +22,43 @@ public class Skelbimai extends AppCompatActivity {
         setContentView(R.layout.skelbimaidesign);
         myTextView = (TextView) findViewById(R.id.textView2);
 
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
 
+            case R.id.action_favorite:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
 
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
 
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.appbarmenu, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView =
+                (SearchView) searchItem.getActionView();
+
+        // Configure the search info and add any event listeners...
+
+        return super.onCreateOptionsMenu(menu);
+    }
 }
